3a2ef3788c71b92e570cdd685eca5fefe507d162
Chang Hoon Kang <whinit@users.noreply.github.com>
2026-04-14 04:34:45 +0900

    init runtime

---
+++ app/src/main/java/com/kakaocloud/library/KakaocloudLibraryApplication.java
@@ -0,0 +1,15 @@
+package com.kakaocloud.library;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@SpringBootApplication
+public class KakaocloudLibraryApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(KakaocloudLibraryApplication.class, args);
+    }
+
+}
